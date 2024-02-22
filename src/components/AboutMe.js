f19dd576bdc6f80e4b42851133f8b9d73c0eb3df
Hoff <daylan.hoff@gmail.com>
2024-02-22 02:00:43 -0700

    Capitalization fix

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,8 +13,8 @@ const AboutMe = () => {
               I am a passionate technology creator and problem solver striving to move the industry to better and greater standards with the support of the people around me! Since a young age, I have always had a strong desire to help build the world to be a better place, and technology has given me the outlet to do just that!
             </p></div>
             <div className="center">
-      <a href="https://github.com/DaylanHoff/resume/blob/main/Daylan_Hoff_Resume.pdf" target="_blank" class="button" rel="noopener noreferrer">View Resume</a>
-    </div>
+              <a href="https://github.com/DaylanHoff/resume/blob/main/Daylan_Hoff_Resume.pdf" target="_blank" class="button" rel="noopener noreferrer">View Resume</a>
+            </div>
           </h2>
         </div>
       </div>
